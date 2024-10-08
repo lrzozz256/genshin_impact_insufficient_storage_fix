af6f33d928db118168c660603fe01005fdc3ae03
lrzozz256 <khosyividpra@gmail.com>
2024-10-08 16:46:46 +0700

    genshin impact

---
+++ giisf.sh
@@ -0,0 +1,58 @@
+#!/system/bin/sh
+old_file=$(ls ~/sdcard/Android/data/com.miHoYo.GenshinImpact/files/cache_versions_* | head -n 1)
+cc=$(echo "$old_file" | grep -o '[0-9]*')
+echo "Genshin Impact Storage Fix"
+sleep 2
+echo ""
+echo "Version: 1.0"
+sleep 1
+echo "By: OzzyDev"
+sleep 3
+echo ""
+echo "--[ Checking You Device ]--"
+sleep 1
+echo ""
+echo "Device: $(getprop ro.product.model)"
+sleep 1
+echo "Brand: $(getprop ro.product.brand)"
+sleep 1
+echo "Build Number: $(getprop ro.build.id)"
+sleep 1
+echo "Android Version: $(getprop ro.build.version.release)"
+sleep 1
+echo "Manufacturer: $(getprop ro.product.manufacturer)"
+sleep 1
+echo "Serial: $(getprop ro.serialno)"
+sleep 1
+echo "Kernel: $(uname) | $(uname -r)"
+sleep 1
+echo "CPU Cores: $(grep -c processor /proc/cpuinfo)"
+sleep 3
+echo ""
+echo "--[ Checking Genshin Impact ]--"
+sleep 1
+echo ""
+echo "Genshin Impact UID: $(cat /sdcard/Android/data/com.miHoYo.GenshinImpact/files/Uidinfo.txt)"
+sleep 1
+echo "Genshin Impact Version: $(echo $cc)_v$(cat /sdcard/Android/data/com.miHoYo.GenshinImpact/files/ScriptVersion)"
+sleep 2
+echo ""
+echo "WARNING: Use this script when downloading Genshin Impact resources!"
+sleep 1
+echo "For the first time (NO IN UPDATE STATE)!"
+echo ""
+sleep 3
+
+f="/sdcard/cache_versions_$(echo $cc)"
+echo "Generating ${f}..."
+sleep 2
+ls /sdcard/Android/data/com.miHoYo.GenshinImpact/files/AssetBundles/blocks/00/ | tee "$file"
+sed -i "s/^/AssetBundles\/blocks\/00\//" "$file"
+sed -i "2i hardware_model_config.json" "$file"
+sleep 2
+echo "Clean up generated file..."
+sleep 1
+grep -v "\.tmp$" "$file" | sed -i "/\.tmp$/d" "$file"
+sed -i 's/\r//g' "$file"
+sed -i '/^$/d' "$file" && printf %s "$(cat "$file")" > "$file"
+echo "Done! Paste the file into your Genshin Impact folder, and enjoy:)"
